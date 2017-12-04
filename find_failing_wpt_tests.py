c073d9c686be8333875e28e78e594e76ccada8c9
vinothkumarn <vinothkumar.nagasayanan@etudiant.univ-rennes1.fr>
2017-12-04 16:40:52 +0100

    failing and total count added

--- find_failing_wpt_tests.py
+++ find_failing_wpt_tests.py
@@ -1,45 +1,50 @@
 import os
 import xlsxwriter
+from os.path import expanduser
 
-rootDir = 'testing/web-platform/meta/'
-count = 0
-row = 0
-col = 0
-temp = ""
-tempmult = ""
-requiredSubdirList = ['content-security-policy','mixed-content','subresource-integrity','cors','x-frame-options','referrer-policy','webauthn','feature-policy','credential-management']
-file = open('testing/web-platform/meta/failing_wpt_tests.txt','w+')
-workbook = xlsxwriter.Workbook('testing/web-platform/meta/failing_wpt_tests.xlsx')
-bold = workbook.add_format({'bold': True})
-for dirName, subdirList, fileList in os.walk(rootDir):
-	midtemp = dirName
-	if midtemp.replace('testing/web-platform/meta/','') in requiredSubdirList:
+def findFailingTests(rootDir ,worksheet):
+	count = 0
+	row = 1
+	col = 0
+	temp = ""
+	tempmult = ""
+	for dirName, subdirList, fileList in os.walk(rootDir):
 		for fname in fileList:
 			if '.ini' in fname:
-				if dirName != temp:
-					#print('\t%s\n' % (dirName.replace('testing/web-platform/meta/','')))
-					tempdir = dirName.replace('testing/web-platform/meta/','')
-					try:
-						if(tempdir.index('/')):
-							tempdir = tempdir[:tempdir.index('/')]
-					except:
-						pass
-					if tempmult != tempdir:
-						file.write('\t%s\n' % (tempdir))
-						worksheet = workbook.add_worksheet()
-						row = 0
-						col = 0
-						count = 0
-						worksheet.write(row,col,tempdir,bold)
-						row+=1
-					temp = dirName
-					tempmult = tempdir
 				count+=1
-				#print('\t%s/%s' % (dirName,fname))
-				file.write('%s,%s/%s\n' % (fname,dirName,fname))
 				worksheet.write(row,col,count)
 				worksheet.write(row,col+1,fname)
+				worksheet.write(row,col+2,dirName.strip(expanduser("~")))
 				row+=1
+	return count
+
+def findCountofTests(rootDir):
+	count = 0
+	rootDir = rootDir.replace('/meta/','/tests/')
+	for dirName, subdirList, fileList in os.walk(rootDir):
+		for fname in fileList:
+			if '.html' in fname or '.htm' in fname:
+				count+=1
+	return count
+home = expanduser("~")
+rootDir = home+'/src/mozilla-central/testing/web-platform/meta/'
+requiredSubdirList = ['content-security-policy','mixed-content','subresource-integrity','cors','x-frame-options','referrer-policy','webauthn','feature-policy','credential-management']
+workbook = xlsxwriter.Workbook('failing_wpt_tests.xlsx')
+bold = workbook.add_format({'bold': True})
+for dirName, subdirList, fileList in os.walk(rootDir,workbook):
+	midtemp = dirName
+	testcount = 0
+	failcount = 0
+	if midtemp.replace(rootDir,'') in requiredSubdirList:
+		testcount = findCountofTests(dirName)
+		worksheet = workbook.add_worksheet(midtemp.replace(rootDir,''))
+		worksheet.write(0,0,midtemp.replace(rootDir,''),bold)
+		failcount = findFailingTests(dirName,worksheet)
+		worksheet.write(0,1,'(failing '+str(failcount)+'/total '+str(testcount)+')',bold)
 workbook.close()
-file.close()
-print('\n%s' % count)
+
+
+
+
+
+
